2781c3104957eb6b4f18d0653d35544e862d4ac9
ajayM1988 <ajubalasore@gmail.com>
2026-08-18 22:40:44 +0530

    Increase disk size from 20 to 40

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_eks_node_group" "node-grp" {
   node_role_arn   = aws_iam_role.worker.arn
   subnet_ids      = data.aws_subnets.default.ids
   capacity_type   = "ON_DEMAND"
-  disk_size       = "20"
+  disk_size       = "40"
   instance_types  = ["c7i-flex.large"]
   labels = tomap({ env = "dev" })
 
